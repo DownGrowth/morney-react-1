33ffed1cd0e336bd778c934ca424a26492cb32d4
BK <1209157041@qq.com>
2022-09-12 22:24:02 +0800

    重构tag

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -47,9 +47,9 @@ function Tags() {
     return (
       <Layout>
         <TagList>
-          {tags.map(tag => <li key={tag}>
-            <Link to={'/tags/'+tag }>
-            <span className="oneLine">{tag}</span>
+          {tags.map(tag => <li key={tag.id}>
+            <Link to={'/tags/'+tag.id }>
+            <span className="oneLine">{tag.name}</span>
               <Icon name="right" />
               </Link>
           </li>)}
