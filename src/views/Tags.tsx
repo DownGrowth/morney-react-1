cea4a52172b82b9f0fdeb128dbe16fbea36d1102
BK <1209157041@qq.com>
2022-09-12 21:24:49 +0800

    Tag样式

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,10 +1,58 @@
 import React from "react"
 import Layout from 'components/Layout';
+import { useTags } from "useTags";
+import styled from 'styled-components';
+import Icon from '../components/icon';
+
+const Button=styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 4px;
+  background: #929292;
+  color:white;
+`
+const Center=styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+const Space=styled.div`
+  height: 18px;
+`
+const TagList=styled.ol`
+  font-size: 16px;
+  background: white;
+  >li{
+    //#e5e5e7
+    border-bottom:1px solid #d5d5d5 ;
+    line-height: 20px;
+    padding: 12px 16px 12px 12px;
+    margin-left:16px;
+    margin-right: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
 
 function Tags() {
+ const {tags,setTags}= useTags()
     return (
       <Layout>
-        <h2>标签页面</h2>
+        <TagList>
+          {tags.map(tag => <li key={tag}>
+            <span className="oneLine">{tag}</span>
+            <Icon name="right"/>
+          </li>)}
+        </TagList>
+        <Center>
+          <Space />
+          <Space/>
+          <Button>新增标签</Button>
+          <Space/>
+        </Center>
     </Layout>
     ) 
 }
